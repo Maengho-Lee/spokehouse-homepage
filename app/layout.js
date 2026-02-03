89b7cb324d27c8d8358a778896f89c4615e980ef
leemaengho <leemaengho@spokehouse.co.kr>
2026-02-03 10:48:29 +0900

    google add tag

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2P6WX6Q');`}
+        </Script>
+        {/* End Google Tag Manager */}
+
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17887447554"
           strategy="afterInteractive"
@@ -27,7 +38,12 @@ export default function RootLayout({ children }) {
         </Script>
       </head>
       <body className="bg-gray-100 min-h-screen">
-        <Navbar />
+        <Navbar />        
+        <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2P6WX6Q"
+        height="0" width="0" style="display:none;visibility:hidden">
+        </iframe>
+        </noscript>
         <main>{children}</main>
         <Footer />
       </body>
